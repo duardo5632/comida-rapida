1270d446f2d26e448ca1bb800527a156e6760a0b
Eduardo <duardo5632@gmail.com>
2025-04-20 18:00:30 -0400

    arquitectura limpia bien implementada ahora vamos a hacer el testing2

--- src/infrastructure/api/AuthAPI.ts
+++ src/infrastructure/api/AuthAPI.ts
@@ -2,24 +2,20 @@ const API_URL = import.meta.env.VITE_API_URL; //URL de la API, se obtiene de las
 import axios from 'axios';
 import { UserRepository } from "../../domain/repositories/UserRepository";
 
-//esto no puede esar aca
+//hay otra forma de hacerlo?
 interface LoginAPIResponse {
     exito: boolean; // ✅ Cambié el nombre de la propiedad a "exito" para que coincida con la respuesta del API 
 }
 
-//ver bien
 export const authAPI: UserRepository = {
     async login(name: string, password: string): Promise<boolean> {
       try {
-        const response = await axios.post<LoginAPIResponse>(`${API_URL}/login`, {
-          name,
-          password
-        });
+        const response = await axios.post<LoginAPIResponse>(`${API_URL}/login`, {name,password});
   
         return response.data.exito === true;
       } catch (error) {
         throw new Error("Error al conectarse al servidor, Por favor intente más tarde");
       }
     }
-  };
+};
   
