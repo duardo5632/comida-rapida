c968330a22b5532027e071881db0c28a5e8e15a1
Eduardo <duardo5632@gmail.com>
2025-05-04 16:57:07 -0400

    guardando para modificar

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,5 +7,5 @@ test("Verificar si el inicio de seccion sirve o no", async ({ page }) => {
   await page.getByRole("textbox", { name: "Nombre" }).press("Enter");
   await page.getByRole("textbox", { name: "Contraseña" }).fill("tnt5043849");
   await page.getByRole("textbox", { name: "Contraseña" }).press("Enter");
-  await expect(page).toHaveURL("/menu");
+  /* await expect(page).toHaveURL("/menu"); */
 });
